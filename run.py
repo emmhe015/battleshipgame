fd9dc92577803d5a7508dcf1d28be5a619bd4580
Emma Henriksson <emmaha.eh@gmail.com>
2024-07-14 15:47:44 +0000

    Create create_board function

--- run.py
+++ run.py
@@ -1,1 +1,10 @@
 import random
+
+def create_board(size):
+    """
+    Creates a game board of given size.
+    size (int): The size of the grid.
+    Returns:
+    list: A 2D list representing the game board.
+    """
+    return [["O"] * size for _ in range(size)]
