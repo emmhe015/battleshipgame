9cabff6118b5c5c5ca0b9ee0e32169d33abd851d
Emma Henriksson <emmaha.eh@gmail.com>
2024-07-14 16:02:17 +0000

    Add logic to place_ships

--- run.py
+++ run.py
@@ -20,3 +20,16 @@ def print_board(board):
         print(" ".join(row))
 
 def place_ships(board, size, num_ships):
+    """
+    Places a specified number of ships randomly on the board.
+    board (list): The game board to place ships on.
+    size (int): The size of the grid.
+    num_ships (int): The number of ships to place.
+    """
+    ships = 0
+    while ships < num_ships:
+        ship_row = random.randint(0, size-1)
+        ship_col = random.randint(0, size-1)
+        if board[ship_row][ship_col] != "S":
+            board[ship_row][ship_col] = "S"
+            ships += 1
